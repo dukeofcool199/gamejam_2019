efd40f178e90cf58ab5a4c2b843bb375cf689f27
808Chase <deslauri19@up.edu>
2019-01-27 17:59:20 -0800

    final commit

---
+++ Assets/My Assets/ship_controll.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ship_controll : MonoBehaviour
+{
+    public Camera playerCam;
+    public Camera shipCam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        shipCam.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+     void OnTriggerEnter(Collider other)
+    {
+        if(other.name == "player"){
+        playerCam.enabled = false;
+        shipCam.enabled = true;
+        
+        }
+
+    }
+}
